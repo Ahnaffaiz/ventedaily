dcbcb9970792edc55382fde04ff4c37038500c95
Ahnaffaiz <faiz.putra553@gmail.com>
2025-03-20 10:46:55 +0700

    database: seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,17 +20,26 @@ class DatabaseSeeder extends Seeder
             'password' => Hash::make('password'),
         ]);
 
-        $this->call(ColorSeeder::class);
-        $this->call(SizeSeeder::class);
-        $this->call(BankSeeder::class);
-        $this->call(TermOfPaymentSeeder::class);
-        $this->call(SettingSeeder::class);
-        $this->call(GroupSeeder::class);
-        $this->call(SupplierSeeder::class);
-        $this->call(CategorySeeder::class);
-        $this->call(ProductSeeder::class);
-        $this->call(ProductStockSeeder::class);
-        $this->call(CustomerSeeder::class);
-        $this->call(MarketplaceSeeder::class);
+        if (env('APP_ENV') === 'local') {
+            $this->call(ColorSeeder::class);
+            $this->call(SizeSeeder::class);
+            $this->call(BankSeeder::class);
+            $this->call(TermOfPaymentSeeder::class);
+            $this->call(SettingSeeder::class);
+            $this->call(GroupSeeder::class);
+            $this->call(SupplierSeeder::class);
+            $this->call(CategorySeeder::class);
+            $this->call(ProductSeeder::class);
+            $this->call(ProductStockSeeder::class);
+            $this->call(CustomerSeeder::class);
+            $this->call(MarketplaceSeeder::class);
+        } else {
+            $this->call(BankSeeder::class);
+            $this->call(TermOfPaymentSeeder::class);
+            $this->call(SettingSeeder::class);
+            $this->call(GroupSeeder::class);
+            $this->call(MarketplaceSeeder::class);
+        }
+
     }
 }

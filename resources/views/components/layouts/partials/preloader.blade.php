123248ad4097ca958c5966d8d90424edf1f78561
Ahnaffaiz <faiz.putra553@gmail.com>
2025-04-30 12:13:19 +0700

    fix: preloader

--- resources/views/components/layouts/partials/preloader.blade.php
+++ resources/views/components/layouts/partials/preloader.blade.php
@@ -1,5 +1,5 @@
 <!-- Preloader -->
-<div id="preloader" class="fixed inset-0 z-[9999] flex items-center justify-center bg-white dark:bg-slate-900 transition-opacity duration-500 opacity-100">
+<div id="preloader" class="fixed inset-0 z-[9999] flex items-center justify-center bg-white dark:bg-gray-600 transition-opacity duration-500 opacity-100">
     <div class="flex flex-col items-center">
         <div class="w-16 h-16 border-4 border-dashed rounded-full animate-spin border-primary dark:border-primary"></div>
         <p class="mt-4 text-sm text-slate-600 dark:text-slate-300">Loading...</p>

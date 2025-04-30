10893646455edf49ea1379dc336c9618d9f492f5
Ahnaffaiz <faiz.putra553@gmail.com>
2025-04-30 13:01:27 +0700

    feat: export product stock

--- app/Exports/ProductStockExport.php
+++ app/Exports/ProductStockExport.php
@@ -15,7 +15,15 @@ class ProductStockExport implements FromView
     public function __construct()
     {
         $this->setting = Setting::first();
-        $this->productStocks = ProductStock::with(['product', 'color', 'size'])->get();
+        $this->productStocks = ProductStock::with(['product', 'color', 'size'])
+            ->join('products', 'product_stocks.product_id', '=', 'products.id')
+            ->join('colors', 'product_stocks.color_id', '=', 'colors.id')
+            ->join('sizes', 'product_stocks.size_id', '=', 'sizes.id')
+            ->select('product_stocks.*')
+            ->orderBy('products.name')
+            ->orderBy('colors.name')
+            ->orderBy('sizes.name')
+            ->get();
     }
 
     public function view(): View
